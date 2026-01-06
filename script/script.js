ac4bc08bb72c394e993547a85a24c9ead571cf1a
bergsonall <bruninhodubal@hotmail.com>
2026-01-06 00:31:05 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,3 +7,22 @@ window.addEventListener('scroll', function() {
         nav.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
     }
 });
+
+// Configuração do LoginRadius
+var loginRadiusV2 = loginRadiusV2 || {};
+loginRadiusV2.util = loginRadiusV2.util || {};
+loginRadiusV2.util.ready = function(callback) {
+    if (/complete|interactive|loaded/.test(document.readyState)) {
+        callback();
+    } else {
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+};
+
+loginRadiusV2.util.ready(function() {
+    LoginRadiusSDK.setLoginRadiusConfig("ramosbarbearia", {
+        hashKey: "1f40c717-bc79-4ea4-a7d6-8e22db12788c",
+        apiKey: "sidzrYtYiLtOZ4AQMH5bYMGqVciYeR5MdNe8BHu7yZM",
+        v2Flows: true
+    });
+});
